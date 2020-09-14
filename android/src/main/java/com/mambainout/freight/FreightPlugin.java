c797841f727b20a6e1934de962641f47d29c876b
王煜 <hack888888889@vip.qq.com>
2020-09-14 11:30:06 +0800

    移除android 依赖信息和 插件实现

--- android/src/main/java/com/mambainout/freight/FreightPlugin.java
+++ android/src/main/java/com/mambainout/freight/FreightPlugin.java
@@ -1,201 +1,24 @@
 package com.mambainout.freight;
 
-import android.app.Activity;
 
 import androidx.annotation.NonNull;
 
-import io.flutter.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
-import com.hdgq.locationlib.LocationOpenApi;
-import com.hdgq.locationlib.entity.ShippingNoteInfo;
-import com.hdgq.locationlib.listener.OnResultListener;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * FreightPlugin
  */
-public class FreightPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+public class FreightPlugin implements FlutterPlugin {
 
-    private AMapLocationClient locationClient;
-    private Activity activity;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        final MethodChannel channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "freight");
-        channel.setMethodCallHandler(this);
+
     }
 
-    @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull final Result result) {
-        switch (call.method) {
-            case "init":
-                String appId = call.argument("appId");
-                String appSecurity = call.argument("appSecurity");
-                String eeCode = call.argument("eeCode");
-                String env = call.argument("env");
-                LocationOpenApi.init(activity, appId, appSecurity, eeCode, env, new OnResultListener() {
-                    @Override
-                    public void onSuccess() {
-                        HashMap<String, String> json = new HashMap<>();
-                        json.put("code", "0");
-                        json.put("message", "服务初始化成功");
-                        result.success(json);
-                    }
-
-                    @Override
-                    public void onFailure(String code, String message) {
-                        result.error(code, message, null);
-                    }
-                });
-                break;
-            case "start": {
-                List<Map<String, String>> ships = call.argument("ships");
-                assert ships != null;
-                ShippingNoteInfo[] shipList = new ShippingNoteInfo[ships.size()];
-                for (int i = 0; i < shipList.length; i++) {
-                    Log.d("ship", ships.get(i).toString());
-                    ShippingNoteInfo ship = new ShippingNoteInfo();
-                    ship.setShippingNoteNumber(ships.get(i).get("shippingNoteNumber"));
-                    ship.setSerialNumber(ships.get(i).get("serialNumber"));
-                    ship.setStartCountrySubdivisionCode(ships.get(i).get("startCountrySubdivisionCode"));
-                    ship.setEndCountrySubdivisionCode(ships.get(i).get("endCountrySubdivisionCode"));
-                    shipList[i] = ship;
-                }
-
-                LocationOpenApi.start(this.activity, shipList, new OnResultListener() {
-                    @Override
-                    public void onSuccess() {
-                        HashMap<String, String> json = new HashMap<String, String>();
-                        json.put("code", "0");
-                        json.put("message", "装车成功");
-                        result.success(json);
-                    }
-
-                    @Override
-                    public void onFailure(String code, String message) {
-                        result.error(code, message, null);
-
-                    }
-                });
-                break;
-            }
-            case "stop": {
-                List<Map<String, String>> ships = call.argument("ships");
-                ShippingNoteInfo[] shipList = new ShippingNoteInfo[ships.size()];
-                for (int i = 0; i < shipList.length; i++) {
-                    Log.d("ship", ships.get(i).toString());
-                    ShippingNoteInfo ship = new ShippingNoteInfo();
-                    ship.setShippingNoteNumber(ships.get(i).get("shippingNoteNumber"));
-                    ship.setSerialNumber(ships.get(i).get("serialNumber"));
-                    ship.setStartCountrySubdivisionCode(ships.get(i).get("startCountrySubdivisionCode"));
-                    ship.setEndCountrySubdivisionCode(ships.get(i).get("endCountrySubdivisionCode"));
-                    shipList[i] = ship;
-                }
-
-                LocationOpenApi.stop(this.activity, shipList, new OnResultListener() {
-                    @Override
-                    public void onSuccess() {
-                        HashMap<String, String> json = new HashMap<>();
-                        json.put("code", "0");
-                        json.put("message", "stop成功");
-                        result.success(json);
-                    }
-
-                    @Override
-                    public void onFailure(String code, String message) {
-                        result.error(code, message, null);
-                    }
-                });
-                break;
-            }
-            case "initAmap":
-                Log.d("freight.Android", "initAmap");
-
-                synchronized (this) {
-                    if (locationClient == null) {
-                        // 初始化client
-                        locationClient = new AMapLocationClient(this.activity);
-
-                        // 新建定位参数
-                        AMapLocationClientOption option = new AMapLocationClientOption();
-                        // 定位场景 签到
-                        option.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
-                        // 定位模式 高精度
-                        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-                        // 单次定位
-                        option.setOnceLocation(true);
-                        // 地址描述 否
-                        option.setNeedAddress(false);
-
-                        // 设置定位参数
-                        locationClient.setLocationOption(option);
-                    }
-                }
-
-                result.success(true);
-                break;
-            case "getLocation":
-                Log.d("freight.Android", "getLocation");
-
-                synchronized (this) {
-                    final AMapLocationListener listener = new AMapLocationListener() {
-                        @Override
-                        public void onLocationChanged(AMapLocation aMapLocation) {
-                            Log.d("freight.Android.getLocation", "定位结果..." + aMapLocation);
-                            Map<String, Double> map = new HashMap<>();
-                            map.put("latitude", aMapLocation.getLatitude());
-                            map.put("longitude", aMapLocation.getLongitude());
-
-                            result.success(map);
-
-                            locationClient.stopLocation();
-                        }
-                    };
-
-                    locationClient.setLocationListener(listener);
-                    locationClient.startLocation();
-                }
-                break;
-            default:
-                result.notImplemented();
-                break;
-        }
-    }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     }
 
-    @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-        if (activity == null) {
-            activity = binding.getActivity();
-        }
-    }
-
-    @Override
-    public void onDetachedFromActivityForConfigChanges() {
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-    }
 }
--- lib/freight.dart
+++ lib/freight.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
+///仅有 ios
+///android 实现在appp  因为和 amap冲突
 class Freight {
   static const MethodChannel _channel = const MethodChannel('freight');
 
@@ -11,32 +14,32 @@ class Freight {
   }
 
   /// 初始化服务
-  static Future<Map<String, dynamic>> init(
-      String appId, String appSecurity, String eeCode, String env) async {
-    final Map<dynamic, dynamic> result = await _channel.invokeMethod("init", {
-      "appId": appId,
-      "appSecurity": appSecurity,
-      "eeCode": eeCode,
-      "env": env
-    });
+  static Future<Map<String, dynamic>> init(String appId, String appSecurity, String eeCode, String env) async {
+    if (Platform.isAndroid) {
+      return null;
+    }
+    final Map<dynamic, dynamic> result =
+        await _channel.invokeMethod("init", {"appId": appId, "appSecurity": appSecurity, "eeCode": eeCode, "env": env});
 
     return new Map<String, dynamic>.from(result);
   }
 
   /// 启用定位
-  static Future<Map<String, dynamic>> start(
-      List<Map<String, dynamic>> ships) async {
-    final Map<dynamic, dynamic> result =
-        await _channel.invokeMethod("start", {"ships": ships});
+  static Future<Map<String, dynamic>> start(List<Map<String, dynamic>> ships) async {
+    if (Platform.isAndroid) {
+      return null;
+    }
+    final Map<dynamic, dynamic> result = await _channel.invokeMethod("start", {"ships": ships});
 
     return new Map<String, dynamic>.from(result);
   }
 
   /// 结束定位
-  static Future<Map<String, dynamic>> stop(
-      List<Map<String, dynamic>> ships) async {
-    final Map<dynamic, dynamic> result =
-        await _channel.invokeMethod("stop", {"ships": ships});
+  static Future<Map<String, dynamic>> stop(List<Map<String, dynamic>> ships) async {
+    if (Platform.isAndroid) {
+      return null;
+    }
+    final Map<dynamic, dynamic> result = await _channel.invokeMethod("stop", {"ships": ships});
 
     return new Map<String, dynamic>.from(result);
   }
@@ -45,11 +48,17 @@ class Freight {
   /// 备注: 仅在iOS平台下调用
   /// @param 高德地图服务key
   static Future<bool> initAmap(String key) async {
+    if (Platform.isAndroid) {
+      return null;
+    }
     return await _channel.invokeMethod("initAmap", {"key": key});
   }
 
   /// 获取定位信息
   static Future<Map<String, dynamic>> getLocation() async {
+    if (Platform.isAndroid) {
+      return null;
+    }
     Map<dynamic, dynamic> result = await _channel.invokeMethod("getLocation");
     return new Map.from(result);
   }

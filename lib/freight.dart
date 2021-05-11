3f853cd72ba8126b862d0e400b685d8da06ac84f
wangyi <hack888888889@vip.qq.com>
2021-05-11 14:36:30 +0800

    merge null safe

--- lib/freight.dart
+++ lib/freight.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/services.dart';
 
@@ -8,29 +7,29 @@ import 'package:flutter/services.dart';
 class Freight {
   static const MethodChannel _channel = const MethodChannel('freight');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
   /// 初始化服务
   static Future<Map<String, dynamic>> init(String appId, String appSecurity, String eeCode, String env) async {
     final Map<dynamic, dynamic> result =
-        await _channel.invokeMethod("init", {"appId": appId, "appSecurity": appSecurity, "eeCode": eeCode, "env": env});
+        await (_channel.invokeMethod("init", {"appId": appId, "appSecurity": appSecurity, "eeCode": eeCode, "env": env}) as FutureOr<Map<dynamic, dynamic>>);
 
     return new Map<String, dynamic>.from(result);
   }
 
   /// 启用定位
   static Future<Map<String, dynamic>> start(List<Map<String, dynamic>> ships) async {
-    final Map<dynamic, dynamic> result = await _channel.invokeMethod("start", {"ships": ships});
+    final Map<dynamic, dynamic> result = await (_channel.invokeMethod("start", {"ships": ships}) as FutureOr<Map<dynamic, dynamic>>);
 
     return new Map<String, dynamic>.from(result);
   }
 
   /// 结束定位
   static Future<Map<String, dynamic>> stop(List<Map<String, dynamic>> ships) async {
-    final Map<dynamic, dynamic> result = await _channel.invokeMethod("stop", {"ships": ships});
+    final Map<dynamic, dynamic> result = await (_channel.invokeMethod("stop", {"ships": ships}) as FutureOr<Map<dynamic, dynamic>>);
 
     return new Map<String, dynamic>.from(result);
   }
@@ -38,13 +37,13 @@ class Freight {
   /// 初始化高德地图服务
   /// 备注: 仅在iOS平台下调用
   /// @param 高德地图服务key
-  static Future<bool> initAmap(String key) async {
+  static Future<bool?> initAmap(String key) async {
     return await _channel.invokeMethod("initAmap", {"key": key});
   }
 
   /// 获取定位信息
   static Future<Map<String, dynamic>> getLocation() async {
-    Map<dynamic, dynamic> result = await _channel.invokeMethod("getLocation");
+    Map<dynamic, dynamic> result = await (_channel.invokeMethod("getLocation") as FutureOr<Map<dynamic, dynamic>>);
     return new Map.from(result);
   }
 }
